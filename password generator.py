f1956443c131705f6d069ff17021f728b77b86b1
Rinku180 <159286788+Rinku180@users.noreply.github.com>
2024-02-24 20:54:28 +0530

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import string
+import random
+if __name__=="__main__":
+    s1=string.ascii_lowercase
+    s2=string.ascii_uppercase
+    s3=string.digits
+    s4=string.punctuation
+    passlen=int(input(("enter the length of the password:")))
+    s=[]
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    print("your password is: ")
+    print("".join(random.sample(s, passlen)))
+    
+
+
+# In[ ]:
+
+
+
+
